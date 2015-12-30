48a402c65a45a99e72102ad93a12b5c0191f61c1
aidinabedi <aidinabedi@gmail.com>
2015-12-30 06:53:45 +0100

    minor improvements and code cleanup.

--- CRankKicker.cs
+++ CRankKicker.cs
@@ -33,7 +33,7 @@ namespace PRoConEvents
 
 		public string GetPluginVersion()
 		{
-			return "1.0.0.0";
+			return "1.1.0.0";
 		}
 
 		public string GetPluginAuthor()
@@ -70,7 +70,7 @@ namespace PRoConEvents
 
 		public void OnPluginLoaded(string hostName, string port, string proconVersion)
 		{
-			RegisterEvents(GetType().Name, "OnPlayerJoin", "OnListPlayers", "OnReservedSlotsPlayerAdded", "OnReservedSlotsPlayerRemoved", "OnReservedSlotsList", "OnReservedSlotsCleared");
+			RegisterEvents(className, "OnPlayerJoin", "OnListPlayers", "OnReservedSlotsPlayerAdded", "OnReservedSlotsPlayerRemoved", "OnReservedSlotsList", "OnReservedSlotsCleared");
 		}
 
 		public void OnPluginEnable()
@@ -82,14 +82,16 @@ namespace PRoConEvents
 			ExecuteCommand("procon.protected.send", "admin.listPlayers", "all");
 
 			ExecuteCommand("procon.protected.pluginconsole.write", "^b" + GetPluginName() + " ^2Enabled!" );
+
+			_UpdateCheckInterval();
 		}
 
 		public void OnPluginDisable()
 		{
 			m_isPluginEnabled = false;
 			m_reservedPlayers.Clear();
-			
-            ExecuteCommand("procon.protected.tasks.remove", className);
+
+			ExecuteCommand("procon.protected.tasks.remove", className);
 
 			ExecuteCommand("procon.protected.pluginconsole.write", "^b" + GetPluginName() + " ^1Disabled =(" );
 		}
@@ -103,7 +105,7 @@ namespace PRoConEvents
 			else if (variable == "Check Interval")
 			{
 				int.TryParse(value, out m_checkInterval);
-				UpdateCheckInterval();
+				_UpdateCheckInterval();
 			}
 			else if (variable == "Allow Reserved Players" && Enum.IsDefined(typeof(enumBoolYesNo), value))
 			{
@@ -111,7 +113,7 @@ namespace PRoConEvents
 			}
 		}
 
-		private void UpdateCheckInterval() 
+		private void _UpdateCheckInterval() 
 		{
 			ExecuteCommand("procon.protected.tasks.remove", className);
 
@@ -198,10 +200,10 @@ namespace PRoConEvents
 					int rank = player.Rank;
 					if (rank == 0)
 					{
-						rank = GetGameTrackerBC2Stats(soldierName);
+						rank = _GetGameTrackerBC2Stats(soldierName);
 						if (rank == 0)
 						{
-							rank = GetBFBCStatRank(soldierName);
+							rank = _GetBFBCStatRank(soldierName);
 							if (rank == 0) continue;
 						}
 					}
@@ -219,28 +221,34 @@ namespace PRoConEvents
 			}
 		}
 
-		private int GetGameTrackerBC2Stats(string soldierName)
+		private void _KickPlayer(string soldierName, int rank)
+		{
+			ExecuteCommand("procon.protected.send", "admin.kickPlayer", soldierName, "You got kicked due to your Player Rank (" + rank + ") being too high!");
+			ExecuteCommand("procon.protected.send", "admin.say", "Kicked '" + soldierName + "' because their Player Rank (" + rank + ") is too high!");
+		}
+
+		private int _GetGameTrackerBC2Stats(string soldierName)
 		{
 			const string rankPrefix = "<img src=\"/images/bc2/r0";
-		
+
 			try
 			{
 				string result;
-			
+
 				/* Getting GameTracker BC2 stats */
 				using (var wc = new WebClient())
 				{
-					string url = "http://www.gametracker.com/games/bc2/stats/" + UrlEncode(soldierName) + "/";
-					
+					string url = "http://www.gametracker.com/games/bc2/stats/" + _UrlEncode(soldierName) + "/";
+
 					result = wc.DownloadString(url);
 					if (String.IsNullOrEmpty(result)) return 0;
 				}
 
 				int index = result.IndexOf(rankPrefix, 0, result.Length - 2, StringComparison.OrdinalIgnoreCase);
 				if (index < 0) return 0;
-				
+
 				string rankString = result.Substring(index + rankPrefix.Length, 2);
-				
+
 				int rank;
 				return int.TryParse(rankString, out rank) ? rank : 0;
 			}
@@ -251,17 +259,17 @@ namespace PRoConEvents
 			}
 		}
 
-		private int GetBFBCStatRank(string soldierName)
+		private int _GetBFBCStatRank(string soldierName)
 		{
 			try
 			{
 				string result;
-			
+
 				/* Getting BFBC2 stats */
 				using (var wc = new WebClient())
 				{
-					string url = "http://api.bfbcs.com/api/pc?players=" + UrlEncode(soldierName) + "&fields=smallinfo";
-					
+					string url = "http://api.bfbcs.com/api/pc?players=" + _UrlEncode(soldierName) + "&fields=smallinfo";
+
 					result = wc.DownloadString(url);
 					if (String.IsNullOrEmpty(result)) return 0;
 				}
@@ -287,33 +295,33 @@ namespace PRoConEvents
 				return 0;
 			}
 		}
-		
-		private static string UrlEncode(string text)
+
+		private static string _UrlEncode(string text)
 		{
-            var sb = new StringBuilder(text.Length * 6);
-            
-            foreach (char c in text)
-            {
-                if (('0' <= c && c <= '9') || ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || (c == '*' || c == '-' || c == '.' || c == '_'))
-                {
-                    sb.Append(c);
-                }
-                else if (c <= '\u007F')
-                {
-                    sb.Append('%');
-                    sb.Append(((int)c).ToString("X2"));
-                }
-                else
-                {
-                    foreach (byte b in Encoding.UTF8.GetBytes(c.ToString()))
-                    {
-                        sb.Append('%');
-                        sb.Append(b.ToString("X2"));
-                    }
-                }
-            }
-            
-            return sb.ToString();
+			var sb = new StringBuilder(text.Length * 6);
+
+			foreach (char c in text)
+			{
+				if (('0' <= c && c <= '9') || ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || (c == '*' || c == '-' || c == '.' || c == '_'))
+				{
+					sb.Append(c);
+				}
+				else if (c <= '\u007F')
+				{
+					sb.Append('%');
+					sb.Append(((int)c).ToString("X2"));
+				}
+				else
+				{
+					foreach (byte b in Encoding.UTF8.GetBytes(c.ToString()))
+					{
+						sb.Append('%');
+						sb.Append(b.ToString("X2"));
+					}
+				}
+			}
+
+			return sb.ToString();
 		}
 	}
 }
